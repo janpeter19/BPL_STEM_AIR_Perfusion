55e68d5cc9b50a21e618f5f5f85e8ac4888225da
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2025-11-14 09:53:57 +0100

    Modifed setup.file for lower-case ncp - special for PyFMI 1.9.0?

--- BPL_STEM_AIR_Perfusion_explore.py
+++ BPL_STEM_AIR_Perfusion_explore.py
@@ -59,12 +59,12 @@ elif platform.system() == 'Linux':
 if flag_type in ['CS', 'cs']:
    opts_std = model.simulate_options()
    opts_std['silent_mode'] = True
-   opts_std['NCP'] = 500 
+   opts_std['ncp'] = 500 
    opts_std['result_handling'] = 'binary'     
 elif flag_type in ['ME', 'me']:
    opts_std = model.simulate_options()
    opts_std["CVode_options"]["verbosity"] = 50 
-   opts_std['NCP'] = 500 
+   opts_std['ncp'] = 500 
    opts_std['result_handling'] = 'binary'  
 else:    
    print('There is no FMU for this platform')
