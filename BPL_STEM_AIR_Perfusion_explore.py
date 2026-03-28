52a22469a9f5fb99ed35b38251bd5f150659bc9a
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2026-03-28 09:33:55 +0100

    Update FMU-explore 1.0.3

--- BPL_STEM_AIR_Perfusion_explore.py
+++ BPL_STEM_AIR_Perfusion_explore.py
@@ -11,6 +11,7 @@
 # 2025-11-13 - Removed global declaration outside the funtions
 # 2025-11-14 - FMU-explore 1.0.2 corrected
 # 2025-11-19 - FMU-explore 1.0.2 corrected again parLocation() with sheets as argument
+# 2026-03-28 - FMU-explore 1.0.3
 #------------------------------------------------------------------------------------------------------------------
 
 # Setup framework
@@ -273,7 +274,7 @@ def describe(name, decimals=3):
 
 #------------------------------------------------------------------------------------------------------------------
 #  General code 
-FMU_explore = 'FMU-explore version 1.0.2'
+FMU_explore = 'FMU-explore version 1.0.3'
 #------------------------------------------------------------------------------------------------------------------
 
 # Define function par() for parameter update
@@ -515,6 +516,9 @@ def describe_general(name, decimals, parLocation=parLocation):
       unit = 'h'
       print(description,'[',unit,']')
       
+   elif name == 'process':
+      print(model.get_description())    
+      
    elif name in parLocation.keys():
       description = model.get_variable_description(parLocation[name])
       value = model.get(parLocation[name])[0]
@@ -598,6 +602,13 @@ def system_info():
    print(' -MSL:', MSL_version)    
    print(' -Description:', BPL_version)   
    print(' -Interaction:', FMU_explore)
+   
+def SDG(explanation=False):
+  if explanation:
+    print('"Soli Deo Gloria"')
+    print(' It is latin and means "To the honour of God".') 
+    print(' The great composer Johan Sebastian Bach used to end his compositions with this small remark SDG.')
+    print(' And I like to do that too :).')    
    
 #------------------------------------------------------------------------------------------------------------------
 #  Startup
